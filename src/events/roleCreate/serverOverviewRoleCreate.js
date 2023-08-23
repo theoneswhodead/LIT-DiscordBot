7c0f01fa5f98de3010d11d649f90d1f0bc2785b9
NyMek <nymek.kontakt@gmail.com>
2023-08-23 12:43:43 +0200

    roleCreate roleDelete

---
+++ src/events/roleCreate/serverOverviewRoleCreate.js
@@ -0,0 +1,43 @@
+const serverOverview = require('../../models/serverOverviewModel')
+const formatDate = require('../../functions/formatDate')
+
+module.exports = async (client) => {
+
+
+    try {
+            client.guilds.cache.forEach( async (guild) => {
+                const today = formatDate(new Date())
+
+                const fetchedServerOverview = await serverOverview.findOne({
+                    guildId: guild.id,
+                })
+
+                if(fetchedServerOverview) {
+
+                    await serverOverview.findOneAndUpdate(
+                        {
+                            guildId: guild.id,
+                            'dailyStats.date': today,
+                        },
+                        {
+                            $set: {
+                                'dailyStats.$.roleCount': guild.roles.cache.size,
+                            },
+                        },
+                        { new: true } 
+                    );
+                } else {
+                    console.log('Serwera nie ma w bazie danych')
+                    return;
+                }
+            });
+            
+    } catch (error) {
+        console.log(`Wystąpił błąd podczas zapisu danych do Server Overview`)
+    }
+ 
+}
+
+
+
+
